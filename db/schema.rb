04a62e29ce4dcab80f1f183ef7165af107398298
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2018-03-21 16:23:06 -0600

    Changed the db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180316183240) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
