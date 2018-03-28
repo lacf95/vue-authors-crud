21986a064e68e6c1db7eb00f3c0297f66b9fe3f6
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2018-03-28 12:49:32 -0600

    Refactored Vue router

--- app/javascript/packs/app_vue.js
+++ app/javascript/packs/app_vue.js
@@ -1,14 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import BootstrapVue from 'bootstrap-vue';
 import Status from '../util/status_codes';
 import Session from '../config/session';
 import store from '../config/store';
 import App from '../app.vue';
 
-Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 
 Vue.prototype.$status = Status;
