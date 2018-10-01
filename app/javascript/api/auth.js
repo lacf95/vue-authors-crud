0b4d5c24d6a1fd597c5b04f277734d3e801e3356
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2018-09-30 20:18:22 -0500

    Refactored Auth Model

--- app/javascript/api/auth.js
+++ app/javascript/api/auth.js
@@ -3,11 +3,11 @@ import Model from './model';
 const Auth = new Model();
 
 Auth.signIn = credential => {
-  return Auth.callApi('/v1/sign-in', Auth.self.VERBS.post, credential);
+  return Auth.self.request('/v1/sign-in', Auth.self.VERBS.post, credential);
 }
 
 Auth.signUp = user => {
-  return Auth.callApi('/v1/sign-up', Auth.self.VERBS.post, user);
+  return Auth.self.request('/v1/sign-up', Auth.self.VERBS.post, user);
 }
 
 export default Auth;
